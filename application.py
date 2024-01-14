1446f3b09964527e0eaf7acc4d3b9fcd1b5a549b
jadmin <jlugo71@gmail.com>
2024-01-14 18:52:42 -0400

    templates created

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 application = Flask(__name__)
 
+
 @application.route('/')
-def hello_world():
-    return 'Sup. Subscribe'
+def home():
+    return render_template('home.html')
+
+
+#if __name__ == "__main__":
+#    application.run(host='0.0.0.0', port=80, debug=True)
+    
