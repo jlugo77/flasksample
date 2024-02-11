fbf6af0b0d7cce67b366f29537ad929cdbe44136
jlugo77 <jlugo71@gmail.com>
2024-02-11 18:09:15 -0400

    jlrappv2

--- jlrapp.py
+++ jlrapp.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
 
-application = Flask(__name__)
+jlrapp = Flask(__name__)
 
 
-@application.route('/')
+@jlrapp.route('/')
 def home():
     return render_template('home.html')
 
 
 #if __name__ == "__main__":
-#    application.run(host='0.0.0.0', port=80, debug=True)
-    
+#    jlrapp.run(host='0.0.0.0', port=80, debug=True)
